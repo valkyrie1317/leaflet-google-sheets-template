866accce8c5523276d5e3676a19510280c80053c
Brittany <bkay1317@gmail.com>
2016-11-22 22:47:27 -0500

    Update and rename geojson.php to recycling.php

--- recycling.php
+++ recycling.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQn9bFgDx3oQ6JmhF6lMFyyK7fTT4HLjjMOJycXPP8hR-XyRv_N2-b_rbL21pCxTutmbyCzOkzwBap8/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
